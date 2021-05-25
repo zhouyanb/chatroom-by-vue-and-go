7930258c691c326ab30fc117999267b98e549f9f
saberのmaster <278457198@qq.com>
2021-05-25 18:50:39 +0800

    css add

--- vueproject/src/components/home.vue
+++ vueproject/src/components/home.vue
@@ -1,17 +1,21 @@
 <template>
     <div class="mainbox">
         <div style="height: 50px"></div>
-        <el-col>
-        <el-container>
+        <el-col >
+        <el-container >
             <el-aside width="200px">
-                <el-menu :default-openeds="['1']" background-color="#7eb2e6" class="list">
+                <!--页面左侧在线用户栏-->
+                <el-menu :default-openeds="['1']" background-color="rgb(76 99 123)" class="list">
                     <el-submenu index="1">
-                        <template slot="title">
-                            <i class="el-icon-user-solid"></i>
-                            <span>在线用户</span>
+                        <template slot="title" >
+                            <i class="el-icon-user-solid" ></i>
+                            <span style="color:white">在线用户</span>
                         </template> 
                         <el-menu-item-group>
-                            <el-menu-item v-for="item in userlist" v-bind:key="item.index"><router-link :to="{name:'userhome',params:{username:item}}" class="el-icon-user"></router-link>  {{ item }} </el-menu-item>
+                            <el-menu-item  v-for="item in userlist" v-bind:key="item.index" style="color:white">
+                                <!--<router-link :to="{name:'userhome',params:{username:item}}" class="el-icon-user"></router-link>-->
+                                  {{ item }} 
+                            </el-menu-item>
     
                         </el-menu-item-group>
                     </el-submenu>
@@ -29,11 +33,11 @@
                     </div>    
                 </el-main> 
                 <el-footer height="150px">
-                    <div style="height:110px; width:700px;">
-                        <el-input type="textarea" v-model="content" @keyup.enter.native="putmessage"></el-input>
+                    <div style="height:110px; width:700px; ">
+                        <el-input type="textarea" v-model="content" @keyup.enter.native="putmessage" ></el-input>
                     </div>
                     <div class="buttonbox">
-                        <el-button type="primary" size="mini" @click.native="putmessage">发送</el-button>
+                        <el-button type="primary" size="mini" @click.native="putmessage" style="background-color: #516479;border-color: #516479;">发送</el-button>
                     </div>
                    
                 </el-footer>
@@ -53,6 +57,7 @@ export default{
             content:'',
             messagelist:[],
             userlist:[],
+            // path:"ws://127.0.0.1:60/ws",
             path:"ws://47.99.242.48:60/ws",
             socket:{},
             right_style:{
@@ -77,8 +82,9 @@ export default{
         },
         onopen:function(){
             console.log('连接成功');
-            // this.socket.send({username:this.$store.state.username})
+            this.socket.send({username:this.$store.state.username})
             var username=this.$store.state.username;
+            // var username="saber";
             var json=JSON.stringify(username);
             this.socket.send(json);
         },
@@ -156,24 +162,21 @@ export default{
     border: none;
 }
 #main {
-    background-color: #99CCFF;
-    /* background-color: #7eb2e6; */
+    background-color: #516479;
     text-align: right;
-    
-    /* background-color: #3c9dc0; */
     line-height: 60px;
-    /* box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04) */
 }
 .list {
     height: 600px;
 }
 .el-textarea__inner {
     resize: none;
+    /* border-color: #516479; */
     border: none;
     width:700px;
     height:100px;
-    /* background-color: -internal-light-dark(rgb(255, 255, 255), rgb(255, 255, 255)); */
     padding: 2px;
+    border-radius: 0px
 }
 .buttonbox {
     line-height: 0%;
@@ -182,4 +185,5 @@ export default{
 .el-button {
     margin: 0 10px 0 0;
 }
+
 </style>
