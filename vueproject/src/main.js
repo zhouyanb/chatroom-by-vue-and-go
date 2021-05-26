8af896c8f4e3077e48ada2575525f4d2b0ce5e9e
zhouyanb <502126953@qq.com>
2021-05-26 20:41:23 +0800

    update

--- vueproject/src/main.js
+++ vueproject/src/main.js
@@ -4,16 +4,17 @@ import 'element-ui/lib/theme-chalk/index.css'
 import element from './element/index'
 import axios from 'axios'
 import router from './router'
+import store from './store'
 
 
 
 
 Vue.use(element)
-
 Vue.config.productionTip = false
 Vue.prototype.$ajax=axios
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
 
